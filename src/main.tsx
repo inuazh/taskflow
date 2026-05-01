1c58fcc23b53363f405a1ab0639bbc6a7ad0cc91
inuazh <inuazh.kz@gmail.com>
2026-05-01 17:56:43 +0300

    feat(setup): add react query devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-import { QueryClientProvider } from '@tanstack/react-query'
-import { queryClient } from './app/queryClient.ts'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./app/queryClient.ts";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
- 
-
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <QueryClientProvider client={queryClient}>
-    <App />
+      <ReactQueryDevtools />
+      <App />
     </QueryClientProvider>
   </StrictMode>,
-)
+);
