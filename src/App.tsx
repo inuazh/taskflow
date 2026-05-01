2f36dc36b2ad4cfd456b287ff548579bba245d1d
inuazh <inuazh.kz@gmail.com>
2026-05-01 14:59:09 +0300

    tailwind setting

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
-
 function App() {
-
   return (
     <>
-     
+      <div className="flex min-h-screen items-center justify-center bg-slate-900 text-white">
+        <h1 className="text-4xl font-bold">TaskFlow</h1>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
